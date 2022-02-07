116a08f23c1f36858f258b4d4945e8a5d2c18e1d
USTCcoder <sa231035@mail.ustc.edu.cn>
2022-02-08 00:03:20 +0800

    Site updated: 2022-02-08 00:03:02

---
+++ dist/music.js
@@ -0,0 +1,12 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+      {
+        name: "溯",
+        artist: '马吟吟',
+        url: 'http://www.ytmp3.cn/down/51094.mp3',
+      }
+]
+});
